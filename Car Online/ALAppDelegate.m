8840677c5245cb8e938d9a1276a0ed5706822620
Alex Lebedev <allebedew@gmail.com>
2012-05-21 22:54:34 +0300

    initial

--- Car Online/ALAppDelegate.m
+++ Car Online/ALAppDelegate.m
@@ -14,9 +14,6 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
-    [[NSUserDefaults standardUserDefaults] setObject:@"1e23019c8FdbffB2bec223311e1682" forKey:@"api-key"];
-    [[NSUserDefaults standardUserDefaults] synchronize];
-    
     [[UINavigationBar appearance] setTintColor:[UIColor colorWithWhite:.7 alpha:1.]];
     
     return YES;
